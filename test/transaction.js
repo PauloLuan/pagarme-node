f42913fe053d8f980297355fb8c08c86499c5efa
Paulo Luan <pauloluan.inova@gmail.com>
2015-03-11 13:37:37 -0300

    added more tests to transaction

--- test/transaction.js
+++ test/transaction.js
@@ -4,19 +4,22 @@ var request = require('request');
 var should = require('should');
 var utils = require('./utils');
 
+var validCreditCard = utils.validCreditCard;
+
 describe('Testing Transaction EndPoints', function () {
 
     it('testing /transaction/credit/', function (done) {
         this.timeout(0);
         request.post(utils.url + 'transaction/credit/',
-            { form: utils.validCreditCard },
+            { form: validCreditCard },
 
             function (error, response, body) {
                 var json_body = JSON.parse(body);
                 should(error).not.be.ok;
-                response.statusCode.should.equal(200);
-                json_body.should.have.property('status', 'paid');
-
+                response["statusCode"].should.equal(200);
+                json_body.should.have.property('status');
+                json_body["status"].should.be.equal('paid');
+                json_body["amount"].should.be.equal(validCreditCard.amount);
                 done();
             }
         );
@@ -25,10 +28,15 @@ describe('Testing Transaction EndPoints', function () {
     it('testing /transaction/boleto/', function (done) {
         this.timeout(0);
         request.post(utils.url + 'transaction/boleto/',
-            { form: utils.validCreditCard },
+            { form: validCreditCard },
 
             function (error, response, body) {
-                response.statusCode.should.equal(200);
+                var json_body = JSON.parse(body);
+                should(error).not.be.ok;
+                response["statusCode"].should.equal(200);
+                json_body.should.have.property('status');
+                json_body["status"].should.be.equal('waiting_payment');
+                json_body["amount"].should.be.equal(validCreditCard.amount);
                 done();
             }
         );
