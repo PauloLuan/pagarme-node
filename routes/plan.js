bb9e8a827fa1356d3c593880f92c4f46a72b3f7c
Paulo Luan <pauloluan.inova@gmail.com>
2015-03-11 15:21:13 -0300

    added imports

--- routes/plan.js
+++ routes/plan.js
@@ -2,6 +2,13 @@
 
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var async = require('async');
+
+var utils = require('../utils/utils.js');
+var keys = require('../utils/keys.json');
+
+var api_url = "https://api.pagar.me/1/plans";
 
 router.post('/', function (req, res) {
     var params = req.body;
