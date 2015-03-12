34e6b12d44f37e2d0677738fd27c2d4507bfb066
Paulo Luan <pauloluan.inova@gmail.com>
2015-03-12 09:37:46 -0300

    implementation of plan and subscription route.

--- routes/plan.js
+++ routes/plan.js
@@ -8,21 +8,73 @@ var async = require('async');
 var utils = require('../utils/utils.js');
 var keys = require('../utils/keys.json');
 
-var api_url = "https://api.pagar.me/1/plans";
+var plan_url = "https://api.pagar.me/1/plans/";
+var subscription_url = "https://api.pagar.me/1/subscriptions/";
 
 router.post('/', function (req, res) {
     var params = req.body;
-    return res.send('it works');
+    params = utils.validatePlanData(params);
+
+    request.post({ url: plan_url, form: params }, function (error, response, body) {
+        console.log(body);
+        return res.send(body);
+    });
 });
 
 router.post('/subscriptions/', function (req, res) {
     var params = req.body;
-    return res.send('it works');
+    params = utils.validateSubscriptionData(params);
+
+    utils.getKeyForTransaction(params, function(card_hash){
+        params.card_hash = card_hash;
+
+        request.post({ url: subscription_url, form: params }, function (error, response, body) {
+            console.log(body);
+            return res.send(body);
+        });
+    });
 });
 
 router.post('/subscriptions/cancel', function (req, res) {
     var params = req.body;
-    return res.send('it works');
+    var id = params.id;
+
+    if(id) {
+        var url = subscription_url + id + "/cancel/";
+        params.api_key = keys.api_key;
+
+        request.post({ url: url, form: params }, function (error, response, body) {
+            console.log(body);
+            return res.send(body);
+        });
+    }
+    else {
+        var error = {
+            "errors": [
+            {
+                "type": "missing_parameters",
+                "parameter_name": "id",
+                "message": "coloque o id da assinatura na requisição"
+            }],
+            "url": "/subscriptions/cancel",
+            "method": "post"
+        }
+
+        console.log(body);
+        return res.send(JSON.stringify(error));
+    }
 });
+
+router.post('/subscriptions/postback', function (req, res) {
+    /*
+        TODO: em um ambiente real ele faria alguma modificação no banco,
+        indicando o novo status.
+    */
+    var params = req.body;
+    // database.update(params);
+    console.log(body);
+    return res.send('Thanks for letting me know! :)');
+});
+
 
 module.exports = router;
