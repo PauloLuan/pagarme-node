e0c793d3760cd49dc30aafa7ca211f09301ede67
Paulo Luan <pauloluan.inova@gmail.com>
2015-03-12 09:36:38 -0300

    added tests for plans, including the workflow of a subscription transaction (creation of plan and subscription and cancellation)

--- test/plan.js
+++ test/plan.js
@@ -8,38 +8,61 @@ describe('Testing Plans EndPoints', function () {
 
     it('testing /plan/', function (done) {
         this.timeout(0);
-        request.post(utils.url + 'plan/',
-            { form: utils.validCreditCard },
-
-            function (error, response, body) {
-                response.statusCode.should.equal(200);
-                done();
-            }
-        );
+        createPlan(done);
     });
 
-    it('testing /plan/subscriptions/', function (done) {
+    /*it('testing /plan/subscriptions/', function (done) {
         this.timeout(0);
-        request.post(utils.url + 'plan/subscriptions/',
-            { form: utils.validCreditCard },
-
-            function (error, response, body) {
-                response.statusCode.should.equal(200);
-                done();
-            }
-        );
+        createSubscription(done);
     });
 
     it('testing /plan/subscriptions/cancel/', function (done) {
         this.timeout(0);
-        request.post(utils.url + 'plan/subscriptions/cancel',
-            { form: utils.validCreditCard },
-
-            function (error, response, body) {
-                response.statusCode.should.equal(200);
-                done();
-            }
-        );
-    });
+        cancelSubscription(done);
+    });*/
 
 });
+
+var createPlan = function(done){
+    request.post(utils.url + 'plan/',
+        { form: utils.planData },
+
+        function (error, response, body) {
+            response.statusCode.should.equal(200);
+            var json = JSON.parse(body);
+            console.log("Created plan! ", json.id);
+            createSubscription(json, done);
+        }
+    );
+}
+
+var createSubscription = function(json, done) {
+    var params = utils.subscribeTest;
+    params.plan_id = json.id;
+
+    request.post(utils.url + 'plan/subscriptions/',
+        { form: params },
+
+        function (error, response, body) {
+            response.statusCode.should.equal(200);
+
+            var json = JSON.parse(body);
+            console.log("Created Subscription! ", json.id);
+            cancelSubscription(json, done);
+        }
+    );
+}
+
+var cancelSubscription = function(json, done) {
+    request.post(utils.url + 'plan/subscriptions/cancel',
+        { form: utils.subscribeTest },
+
+        function (error, response, body) {
+            response.statusCode.should.equal(200);
+            var response_json = JSON.parse(body);
+            response_json.status.should.equal('canceled');
+            console.log("Canceled Subscription! ", response_json.id);
+            done();
+        }
+    );
+}
--- test/utils.js
+++ test/utils.js
@@ -4,6 +4,8 @@ var utils = {
     url: 'http://localhost:3000/',
 
     validCreditCard: {
+        postback_url: "http://SeMeuSiteExistisse.com.br/transaction/postback",
+
         card_cvv: '901',
         card_number: '4457000300000007',
         issuing_network: "Visa",
@@ -38,6 +40,42 @@ var utils = {
             ddd:11,
             number:30713261
         }
+    },
+
+    planData: {
+        amount: 5990,
+        days: 30,
+        name: 'Plano Mensal'
+    },
+
+    subscribeTest: {
+        card_cvv: '901',
+        card_number: '4457000300000007',
+        issuing_network: "Visa",
+        card_holder_name: 'Paulo Luan Mariano Silva',
+        card_expiration_date: '0120',
+
+        customer: {
+            name:"Paulo Luan Mariano Silva",
+            document_number: "12345678909",
+            email:"teste@teste.com",
+
+            address: {
+                street:"Av. Brigadeiro Faria Lima",
+                neighborhood:"Jardim Paulistano",
+                zipcode: "01452000",
+                street_number:2941,
+                complementary:"8º andar"
+            },
+
+            phone: {
+                ddd:12,
+                number:991146489
+            },
+
+        },
+
+        postback_url: "http://SeMeuSiteExistisse.com.br/plan/subscriptions/postback"
     },
 
     validCreditCardResponse : {
@@ -172,6 +210,111 @@ var utils = {
             valid: true
         },
         metadata: {}
+    },
+
+    createPlanResponse: {
+        object: "plan",
+        id: 14288,
+        amount: 5990,
+        days: 30,
+        name: "Plano Mensal",
+        trial_days: 0,
+        date_created: "2015-03-11T20:18:45.000Z",
+        payment_methods: [
+            "boleto",
+            "credit_card"
+        ],
+        color: null,
+        charges: null,
+        installments: 1
+    },
+
+    createSubscriptionResponse: {
+        object: "subscription",
+        plan: {
+            object: "plan",
+            id: 14302,
+            amount: 5990,
+            days: 30,
+            name: "Plano Mensal",
+            trial_days: 0,
+            date_created: "2015-03-12T11:31:35.000Z",
+            payment_methods: [
+                "boleto",
+                "credit_card"
+            ],
+            color: null,
+            charges: null,
+            installments: 1
+        },
+        id: 15095,
+        current_transaction: {
+            object: "transaction",
+            status: "paid",
+            refuse_reason: null,
+            status_reason: "acquirer",
+            acquirer_response_code: "00",
+            acquirer_name: "development",
+            authorization_code: "636200",
+            soft_descriptor: null,
+            tid: "1426159898665",
+            nsu: "1426159898665",
+            date_created: "2015-03-12T11:31:38.000Z",
+            date_updated: "2015-03-12T11:31:39.000Z",
+            amount: 5990,
+            installments: 1,
+            id: 186137,
+            cost: 139.85,
+            card_holder_name: "Paulo Luan Mariano Silva",
+            card_last_digits: "0007",
+            card_first_digits: "445700",
+            card_brand: "visa",
+            postback_url: null,
+            payment_method: "credit_card",
+            antifraud_score: null,
+            boleto_url: null,
+            boleto_barcode: null,
+            boleto_expiration_date: null,
+            referer: "api_key",
+            ip: "189.55.109.175",
+            subscription_id: 15095,
+            metadata: {}
+        },
+        postback_url: "http://SeMeuSiteExistisse.com.br/plan/subscriptions/postback",
+        payment_method: "credit_card",
+        card_brand: "visa",
+        card_last_digits: "0007",
+        current_period_start: "2015-03-12T11:31:38.462Z",
+        current_period_end: "2015-04-11T11:31:38.462Z",
+        charges: 0,
+        status: "paid",
+        date_created: "2015-03-12T11:31:38.000Z",
+        phone: null,
+        address: null,
+        customer: {
+            object: "customer",
+            document_number: null,
+            document_type: "cpf",
+            name: null,
+            email: "pauloluan.inova@gmail.com",
+            born_at: null,
+            gender: null,
+            date_created: "2015-03-11T20:39:54.000Z",
+            id: 14538
+        },
+        card: {
+            object: "card",
+            id: "card_ci756xccb001l5m16wwvxeywj",
+            date_created: "2015-03-11T20:39:54.000Z",
+            date_updated: "2015-03-11T20:39:54.000Z",
+            brand: "visa",
+            holder_name: "Paulo Luan Mariano Silva",
+            first_digits: "445700",
+            last_digits: "0007",
+            fingerprint: "pJuL+iaAtw1N",
+            valid: true
+        },
+        metadata: null
     },
 
     api_key: "ak_test_F6Fp2q3wwysq7ndfav4EIajYv8XHuR",
